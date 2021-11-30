fe96f3523023a6992bc658629db3eb5ddf846d7a
Alex Matsson <alexm@student.chalmers.se>
2021-11-30 10:02:55 +0100

    test

--- run_simulation.py
+++ run_simulation.py
@@ -1,13 +1,16 @@
 # Run simulation
 from schedules import Schedule
 
+
 def simulation(stops_fyrktorget, stops_bockkranen, travel_time_fyrktorget, travel_time_bockkranen, depatures_fyrktorget, depatures_bockkranen):
     for depature_fyrktorget, depature_bockkranen in zip(depatures_fyrktorget, depatures_bockkranen):
         steps_fyrktorget = 60 // depature_fyrktorget
         steps_bockkranen = 60 // depature_bockkranen
+        joel = 123
         
         for i in range(60):
             pass
+
 
 def main():
     bus_schedule = Schedule()

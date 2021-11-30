dbeb8530f92d735f0640cbfe4c61f724e79f4c2b
AxelEiman <axeleiman@gmail.com>
2021-11-30 16:23:09 +0100

    Removed old input, added bus stop initialization

--- run_simulation.py
+++ run_simulation.py
@@ -1,21 +1,14 @@
 # Run simulation
-from schedules import Schedule
+import initialize_stops
 
 
-def simulation(stops_fyrktorget, stops_bockkranen, travel_time_fyrktorget, travel_time_bockkranen, depatures_fyrktorget, depatures_bockkranen):
-    for depature_fyrktorget, depature_bockkranen in zip(depatures_fyrktorget, depatures_bockkranen):
-        steps_fyrktorget = 60 // depature_fyrktorget
-        steps_bockkranen = 60 // depature_bockkranen
-        joel = 123
-        
-        for i in range(60):
-            pass
+def simulation():
+    pass
 
 
 def main():
-    bus_schedule = Schedule()
-    stops_fyrktorget, stops_bockkranen, travel_time_fyrktorget, travel_time_bockkranen, depatures_fyrktorget, depatures_bockkranen = bus_schedule.get_schedule()
-    simulation(stops_fyrktorget, stops_bockkranen, travel_time_fyrktorget, travel_time_bockkranen, depatures_fyrktorget, depatures_bockkranen)
+    bstoplist = initialize_stops.initialize_stoplist()
+
 
 if __name__ == '__main__':
     main()

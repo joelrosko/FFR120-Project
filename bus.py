7b5d3ce1a4040e1d38bc20d26c24071979b6aad2
Alex Matsson <alexm@student.chalmers.se>
2021-12-01 09:46:56 +0100

    updated bus.py

--- bus.py
+++ bus.py
@@ -1,20 +1,24 @@
 import numpy as np
+from initialize_stops import initialize_stoplist
 
 
 class Bus:
-    def __init__(self, bus_stops, start_pos, n_passengers=0):
+    # STOP_POSITIONS = np.array([0.0, 0.06544985, 0.39269908, 0.65449847, 0.85084801, 0.91629786, 0.9817477, 1.1126474, 1.24354709, 1.30899694, 1.50534648, 1.63624617, 1.89804556, 2.0943951, 2.15984495, 2.29074464, 2.35619449, 2.55254403, 2.74889357, 2.81434342, 2.94524311, 3.14159265, 3.27249235, 3.33794219, 3.53429174, 3.73064128, 3.79609112, 3.92699082, 3.99244066, 4.1887902, 4.45058959, 4.58148929, 4.77783883, 4.84328867, 4.97418837, 5.10508806, 5.17053791, 5.23598776, 5.4323373, 5.69413668, 6.02138592, 6.08683577])
+
+    def __init__(self, BS_positions, start_pos, n_passengers=0):
         self.angular_velocity = 0.001090277777777778
         self.max_passengers = 10
         self.position = start_pos
         self.n_passengers = n_passengers
         self.at_stop = False
-        self.bus_stops = bus_stops
-        self.next_stop = np.where(start_pos < self.bus_stops)[0][0]  # change
+        self.BS_positions = BS_positions
+        self.next_stop = np.where(start_pos < self.BS_positions)[0][0]  # change
 
 
     def bus_at_stop(self):
-        if (self.bus_stops[self.next_stop] - self.position) < self.angular_velocity:
+        if (self.BS_positions[self.next_stop] - self.position) < self.angular_velocity:
             self.at_stop = True
+            # put the bus positions = bus stop position??
             self.next_stop += 1
         else:
             self.at_stop = False
@@ -30,6 +34,7 @@ class Bus:
     def add_passengers(self):
         if self.n_passengers < self.max_passengers:
             self.n_passengers += 1
+        # passenger_list.append(BusStop.
 
     def remove_passengers(self):
         if self.n_passengers > 0:
@@ -40,7 +45,11 @@ class Bus:
 
 if __name__ == '__main__':
 
-    # bus1 = Bus(1, 5)
+
+    stops = initialize_stoplist()
+    BS_positions = [stops[i].position for i in range(len(stops))]
+    bus1 = Bus(BS_positions, 1, 5)
+
     # for i in range(100):
     #     bus1.add_passengers()
     #     print(bus1.n_free_seats())
@@ -49,30 +58,30 @@ if __name__ == '__main__':
     #
     # print(bus1.n_passengers)
     # print(bus1.at_stop)
-    BUSSTOPS = np.array([0, 0.06544985, 0.39269908, 0.65449847, 0.85084801,
-       0.91629786, 0.9817477 , 1.1126474 , 1.24354709, 1.30899694,
-       1.50534648, 1.63624617, 1.89804556, 2.0943951 , 2.15984495,
-       2.29074464, 2.35619449, 2.55254403, 2.74889357, 2.81434342,
-       2.94524311, 3.14159265])
-
-    start_pos = (np.pi*np.random.randint(180))/180
-
-    # b1 = 0.0
-    # b2 = 0.06545
-    bus1 = Bus(BUSSTOPS, start_pos, 5)
-    # bus1.move_bus()
-    print(bus1.next_stop)
-    print(bus1.position)
+    # BUSSTOPS = np.array([0, 0.06544985, 0.39269908, 0.65449847, 0.85084801,
+    #    0.91629786, 0.9817477 , 1.1126474 , 1.24354709, 1.30899694,
+    #    1.50534648, 1.63624617, 1.89804556, 2.0943951 , 2.15984495,
+    #    2.29074464, 2.35619449, 2.55254403, 2.74889357, 2.81434342,
+    #    2.94524311, 3.14159265])
+    #
+    # start_pos = (np.pi*np.random.randint(360))/180
+    # print(start_pos)
+    # # b1 = 0.0
+    # # b2 = 0.06545
+    # bus1 = Bus(start_pos, 5)
+    # # bus1.move_bus()
+    # # print(bus1.next_stop)
+    # # print(bus1.position)
     # i = 0
     # while True:
-    #
+    #     print(bus1.position)
     #     bus1.move_bus()
     #     print(bus1.position)
-    #     if (BUSSTOPS[i] - bus1.position) < 0.001090277777777778:
+        # if (BUSSTOPS[i] - bus1.position) < 0.001090277777777778:
     #         i += 1
     #         print('at stop')
-    #
-    #
+    stops = initialize_stoplist()
+    bus1.move_bus()
     #
     #
     #

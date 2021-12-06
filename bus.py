f6b0996a52a834603f7470c51b19c1abb6625e86
Alex Matsson <alexm@student.chalmers.se>
2021-12-06 16:23:44 +0100

    updated bus.py

--- bus.py
+++ bus.py
@@ -19,7 +19,7 @@ class Bus:
         self.previous_stop = 0
 
     def bus_at_stop(self):
-        if (np.abs(self.stop_position[self.next_stop] - self.position)) < (self.angular_velocity/2):
+        if (np.abs(self.stop_position[self.next_stop] - self.position)) <= (self.angular_velocity):
             self.previous_stop = self.next_stop
             self.at_stop = True
         else:
@@ -46,56 +46,16 @@ class Bus:
             self.passenger_list.append(current_stop.board_passenger(t))
         else:
             self.boarding_complete()
-        # passenger_list.append(BusStop.
 
     def remove_passenger(self, idx):
         if self.n_passengers > 0:
             self.n_passengers -= 1
             self.passenger_list.pop(idx)
 
+    def __repr__(self):
+        return f'At stop: {self.at_stop} pos: {self.position}: passengers: {self.n_passengers}\n previous stop: ' \
+               f'{self.previous_stop} next stop: {self.next_stop}\n'
         # return self.passenger_list.pop(idx)
-    #def get_nr
 
 
-if __name__ == '__main__':
 
-    print('hej')
-    # stops = initialize_stoplist()
-    # # BS_positions = [stops[i].position for i in range(len(stops))]
-    # bus1 = Bus(stops, 1, 5)
-
-    # for i in range(100):
-    #     bus1.add_passengers()
-    #     print(bus1.n_free_seats())
-    #     print(bus1.n_passengers)
-    #     bus1.remove_passengers()
-    #
-    # print(bus1.n_passengers)
-    # print(bus1.at_stop)
-    # BUSSTOPS = np.array([0, 0.06544985, 0.39269908, 0.65449847, 0.85084801,
-    #    0.91629786, 0.9817477 , 1.1126474 , 1.24354709, 1.30899694,
-    #    1.50534648, 1.63624617, 1.89804556, 2.0943951 , 2.15984495,
-    #    2.29074464, 2.35619449, 2.55254403, 2.74889357, 2.81434342,
-    #    2.94524311, 3.14159265])
-    #
-    # start_pos = (np.pi*np.random.randint(360))/180
-    # print(start_pos)
-    # # b1 = 0.0
-    # # b2 = 0.06545
-    # bus1 = Bus(start_pos, 5)
-    # # bus1.move_bus()
-    # # print(bus1.next_stop)
-    # # print(bus1.position)
-    # i = 0
-    # while True:
-    #     print(bus1.position)
-    #     bus1.move_bus()
-    #     print(bus1.position)
-        # if (BUSSTOPS[i] - bus1.position) < 0.001090277777777778:
-    #         i += 1
-    #         print('at stop')
-
-    #
-    #
-    #
-    #

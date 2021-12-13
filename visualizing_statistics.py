f57cdda9d57fe1fdc8afe0460677e80a293fa6e5
AxelEiman <axeleiman@gmail.com>
2021-12-13 14:45:35 +0100

    Implemented "averaging" for bunching levels, made plots

--- visualizing_statistics.py
+++ visualizing_statistics.py
@@ -42,24 +42,24 @@ def delay_time():
     df2.index = get_timestamps(control2)
     df2.sort_index(inplace=True)
 
-    # scheduled = np.array(load_json('scheduled/delay_times_scheduled1', 'delay_times'))
-    # for i in range(2,6):
-        # scheduled = np.append(scheduled, load_json(f'scheduled/delay_times_scheduled{i}'), axis=0)
-    # df3 = pd.DataFrame({'delay': scheduled[:,0]})
-    # df3.index = get_timestamps(scheduled)
-    # df3.sort_index(inplace=True)
+    scheduled = np.array(load_json('schedule/delay_times_schedule1'))
+    for i in range(2,6):
+        scheduled = np.append(scheduled, load_json(f'schedule/delay_times_schedule{i}'), axis=0)
+    df3 = pd.DataFrame({'delay': scheduled[:,0]})
+    df3.index = get_timestamps(scheduled)
+    df3.sort_index(inplace=True)
 
     roll_nc = df_nc.rolling('600s', min_periods=1).mean()
     roll1 = df1.rolling('600s', min_periods=1).mean()
     roll2 = df2.rolling('600s', min_periods=1).mean()
-    # roll3 = df3.rolling('600s', min_periods=1).mean()
+    roll3 = df3.rolling('600s', min_periods=1).mean()
 
     fig = plt.figure("Passenger delays")
     ax_dt = fig.add_subplot()
     ax_dt.plot(roll_nc, linewidth=2, color='red', label='Uncontrolled')
     ax_dt.plot(roll1, linewidth=2, color='skyblue', label='Front control')
     ax_dt.plot(roll2, linewidth=2, color='g', label='Front and back control')
-    # ax_dt.plot(roll3, linewidth=2, color='y', label='Schedule based')
+    ax_dt.plot(roll3, linewidth=2, color='y', label='Schedule based')
     ax_dt.set_title('Moving average passenger delay over 10 minutes')
     ax_dt.set_xlabel('Time [hours]')
     ax_dt.set_ylabel('Delay [s]')
@@ -68,21 +68,39 @@ def delay_time():
 
 
 def waiting_time():
+    # Not used
     data = load_json('waiting_times', 'waiting_times')
     moving_average = np.convolve(data, np.ones(500), 'valid') / 500
     plt.plot(np.linspace(0,8*3600, len(moving_average)), moving_average, linewidth=2, color='skyblue')
 
 
 def bunching_coef():
-    control = load_json('bunching_coef_control', 'bunching_coef')
-    dubble_control = load_json('bunching_coef_doublecontrol', 'bunching_coef')
-    no_control = load_json('no_control/bunching_coef_nocontrol1', 'bunching_coef')
+    control = np.array(load_json('front/bunching_coef_front1', 'bunching_coef'))
+    for i in range(2,6):
+        control = np.add(control, np.array(load_json(f'front/bunching_coef_front{i}', 'bunching_coef')))
+    control = control/5
+
+    double_control = load_json('front_back/bunching_coef_front_back1', 'bunching_coef')
+    for i in range(2,6):
+        double_control = np.add(double_control, np.array(load_json(f'front_back/bunching_coef_front_back{i}', 'bunching_coef')))
+    double_control = double_control/5
+
+    no_control = load_json('no_control/bunching_coef_no_control1', 'bunching_coef')
+    for i in range(2,6):
+        no_control = np.add(no_control, np.array(load_json(f'no_control/bunching_coef_no_control{i}', 'bunching_coef')))
+    no_control = no_control/5
+
+    scheduled = load_json('schedule/bunching_coef_schedule1', 'bunching_coef')
+    for i in range(2,6):
+        scheduled = np.add(scheduled, np.array(load_json(f'schedule/bunching_coef_schedule{i}', 'bunching_coef')))
+    scheduled = scheduled/5
 
     fig = plt.figure('Bunching levels')
     ax_b = fig.add_subplot()
-    ax_b.plot(np.linspace(0, 8, len(control)), control, linewidth=2, color='skyblue', label='Control')
-    ax_b.plot(np.linspace(0, 8, len(dubble_control)), dubble_control, linewidth=2, color='mediumaquamarine', label='Double Control')
-    ax_b.plot(np.linspace(0, 8, len(no_control)), no_control, linewidth=2, color='red', label='No Control')
+    ax_b.plot(np.linspace(0, 8, len(control)), control, linewidth=2, color='skyblue', label='Front control')
+    ax_b.plot(np.linspace(0, 8, len(double_control)), double_control, linewidth=2, color='g', label='Front and back control')
+    ax_b.plot(np.linspace(0, 8, len(no_control)), no_control, linewidth=2, color='red', label='No control')
+    ax_b.plot(np.linspace(0, 8, len(no_control)), scheduled, linewidth=2, color='y', label='Schedule based')
     ax_b.set_title('Bunching levels, sampled every 10 min')
     ax_b.set_xlabel('Time [h]')
     ax_b.set_ylabel('Variance in distances between buses')
@@ -119,7 +137,7 @@ def waiting_passengers():
 
 def main():
     delay_time()
-    # bunching_coef()
+    bunching_coef()
     # waiting_passengers()
     # var_passengers()
 

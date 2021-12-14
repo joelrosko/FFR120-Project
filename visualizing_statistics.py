ac3110d9fd79ff0ec18e0e40bece15581da46a68
Joel Rosko <joel.rosko97@gmail.com>
2021-12-14 22:59:38 +0100

    Changed font sizes

--- visualizing_statistics.py
+++ visualizing_statistics.py
@@ -21,6 +21,7 @@ def get_timestamps(array):
 
 
 def delay_time():
+    '''
     control1 = np.array(load_json('front/delay_times_front1'))
     for i in range(2,6):
         control1 = np.append(control1, load_json(f'front/delay_times_front{i}'), axis=0)
@@ -34,7 +35,7 @@ def delay_time():
     df_nc = pd.DataFrame({'delay': no_control[:, 0]})
     df_nc.index = get_timestamps(no_control)
     df_nc.sort_index(inplace=True)
-
+    '''
     control2 = np.array(load_json('front_back/delay_times_front_back1'))
     for i in range(2,6):
         control2 = np.append(control2, load_json(f'front_back/delay_times_front_back{i}'), axis=0)
@@ -49,22 +50,24 @@ def delay_time():
     df3.index = get_timestamps(scheduled)
     df3.sort_index(inplace=True)
 
-    roll_nc = df_nc.rolling('600s', min_periods=1).mean()
-    roll1 = df1.rolling('600s', min_periods=1).mean()
+    #roll_nc = df_nc.rolling('600s', min_periods=1).mean()
+    #roll1 = df1.rolling('600s', min_periods=1).mean()
     roll2 = df2.rolling('600s', min_periods=1).mean()
     roll3 = df3.rolling('600s', min_periods=1).mean()
 
     fig = plt.figure("Passenger delays")
     ax_dt = fig.add_subplot()
-    ax_dt.plot(roll_nc, linewidth=2, color='red', label='Uncontrolled')
-    ax_dt.plot(roll1, linewidth=2, color='skyblue', label='Front control')
+    #ax_dt.plot(roll_nc, linewidth=2, color='red', label='Uncontrolled')
+    #ax_dt.plot(roll1, linewidth=2, color='skyblue', label='Front control')
     ax_dt.plot(roll2, linewidth=2, color='g', label='Front and back control')
     ax_dt.plot(roll3, linewidth=2, color='y', label='Schedule based')
-    ax_dt.set_title('Moving average passenger delay over 10 minutes')
-    ax_dt.set_xlabel('Time [hours]')
-    ax_dt.set_ylabel('Delay [s]')
-    ax_dt.legend()
+    ax_dt.set_title('Moving average passenger delay over 10 minutes', fontsize=16)
+    ax_dt.set_xlabel('Time [hours]', fontsize=14)
+    ax_dt.set_ylabel('Delay [s]', fontsize=14)
+    ax_dt.legend(fontsize=12)
     ax_dt.xaxis.set_major_formatter(md.DateFormatter('%H'))
+
+    fig.savefig('figures/new_passenger_delays_best.png', bbox_inches='tight')
 
 
 def waiting_time():
@@ -75,20 +78,24 @@ def waiting_time():
 
 
 def bunching_coef():
+    '''
     control = np.array(load_json('front/bunching_coef_front1', 'bunching_coef'))
     for i in range(2,6):
         control = np.add(control, np.array(load_json(f'front/bunching_coef_front{i}', 'bunching_coef')))
     control = control/5
+    '''
 
     double_control = load_json('front_back/bunching_coef_front_back1', 'bunching_coef')
     for i in range(2,6):
         double_control = np.add(double_control, np.array(load_json(f'front_back/bunching_coef_front_back{i}', 'bunching_coef')))
     double_control = double_control/5
 
+    '''
     no_control = load_json('no_control/bunching_coef_no_control1', 'bunching_coef')
     for i in range(2,6):
         no_control = np.add(no_control, np.array(load_json(f'no_control/bunching_coef_no_control{i}', 'bunching_coef')))
     no_control = no_control/5
+    '''
 
     scheduled = load_json('schedule/bunching_coef_schedule1', 'bunching_coef')
     for i in range(2,6):
@@ -97,15 +104,16 @@ def bunching_coef():
 
     fig = plt.figure('Bunching levels')
     ax_b = fig.add_subplot()
-    ax_b.plot(np.linspace(0, 8, len(control)), control, linewidth=2, color='skyblue', label='Front control')
+    #ax_b.plot(np.linspace(0, 8, len(control)), control, linewidth=2, color='skyblue', label='Front control')
     ax_b.plot(np.linspace(0, 8, len(double_control)), double_control, linewidth=2, color='g', label='Front and back control')
-    ax_b.plot(np.linspace(0, 8, len(no_control)), no_control, linewidth=2, color='red', label='No control')
-    ax_b.plot(np.linspace(0, 8, len(no_control)), scheduled, linewidth=2, color='y', label='Schedule based')
-    ax_b.set_title('Bunching levels, sampled every 10 min')
-    ax_b.set_xlabel('Time [h]')
-    ax_b.set_ylabel('Variance in distances between buses')
-    ax_b.legend()
+    #ax_b.plot(np.linspace(0, 8, len(no_control)), no_control, linewidth=2, color='red', label='No control')
+    ax_b.plot(np.linspace(0, 8, len(scheduled)), scheduled, linewidth=2, color='y', label='Schedule based')
+    ax_b.set_title('Bunching levels, sampled every 10 min', fontsize=16)
+    ax_b.set_xlabel('Time [h]', fontsize=14)
+    ax_b.set_ylabel('Variance in distances between buses', fontsize=14)
+    ax_b.legend(fontsize=12)
 
+    fig.savefig('figures/new_bunching_levels_best.png', bbox_inches='tight')
 
 def var_passengers():
     control = load_json('var_passengers_control', 'var_passengers')
@@ -116,10 +124,10 @@ def var_passengers():
     ax_vp.plot(np.linspace(0, 8, len(control)), control, linewidth=2, color='skyblue')
     ax_vp.plot(np.linspace(0, 8, len(dubble_control)), dubble_control, linewidth=2, color='g')
     ax_vp.plot(np.linspace(0, 8, len(no_control)), no_control, linewidth=2, color='r')
-    ax_vp.set_xlabel('Time [h]')
-    ax_vp.set_ylabel('Variance in passengers on buses')
-    ax_vp.set_title('Passenger variance')
-    ax_vp.legend()
+    ax_vp.set_xlabel('Time [h]', fontsize=14)
+    ax_vp.set_ylabel('Variance in passengers on buses', fontsize=14)
+    ax_vp.set_title('Passenger variance', fontsize=16)
+    ax_vp.legend(fontsize=12)
 
 
 def waiting_passengers():
@@ -130,9 +138,9 @@ def waiting_passengers():
     ax_wp = fig.add_subplot()
     ax_wp.plot(np.linspace(0,8, len(control)), control, linewidth=2, color='skyblue')
     ax_wp.plot(np.linspace(0,8, len(no_control)), no_control, linewidth=2, color='red')
-    ax_wp.set_title('Passengers waiting at stops')
-    ax_wp.set_ylabel('Number of passengers')
-    ax_wp.set_xlabel('Time [h]')
+    ax_wp.set_title('Passengers waiting at stops', fontsize=16)
+    ax_wp.set_ylabel('Number of passengers', fontsize=14)
+    ax_wp.set_xlabel('Time [h]', fontsize=14)
 
 
 def main():
